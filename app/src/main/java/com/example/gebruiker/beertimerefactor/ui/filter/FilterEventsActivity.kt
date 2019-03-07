03054df49c7ae2d01973d8b9d5748c30e36b1279
trOnk12 <m.pachulski94@gmail.com>
2019-03-07 20:24:00 +0100

    title filter added,icon replaced

--- app/src/main/java/com/example/gebruiker/beertimerefactor/ui/filter/FilterEventsActivity.kt
+++ app/src/main/java/com/example/gebruiker/beertimerefactor/ui/filter/FilterEventsActivity.kt
@@ -2,10 +2,18 @@ package com.example.gebruiker.beertimerefactor.ui.filter
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 import com.example.gebruiker.beertimerefactor.R
 import com.example.gebruiker.beertimerefactor.ui.map.MapsActivity
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_filter_event.*
+import android.view.ViewTreeObserver
 
 class FilterEventsActivity : DaggerAppCompatActivity() {
     companion object {
@@ -16,7 +24,10 @@ class FilterEventsActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_filter_event)
+
+        event_enviroment_options.viewTreeObserver.addOnGlobalLayoutListener { (event_enviroment_options.selectedView as TextView).setTextColor(Color.WHITE) }
+        event_type_options.viewTreeObserver.addOnGlobalLayoutListener { (event_type_options.selectedView as TextView).setTextColor(Color.WHITE) }
+
     }
 }
