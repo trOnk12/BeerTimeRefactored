b0a1a5df7b6493af9fbeeeec8bb3929b6e3b2b2b
trOnk12 <m.pachulski94@gmail.com>
2019-06-26 22:06:52 +0200

    functionality almost completed, commit before refactoring

--- app/src/main/java/com/example/gebruiker/beertimerefactor/BaseActivity.kt
+++ app/src/main/java/com/example/gebruiker/beertimerefactor/BaseActivity.kt
@@ -2,10 +2,13 @@ package com.example.gebruiker.beertimerefactor
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.gebruiker.beertimerefactor.baseMVP.BasePresenter
 import com.example.gebruiker.beertimerefactor.baseMVP.BaseView
 import dagger.android.support.DaggerAppCompatActivity
+import kotlin.coroutines.experimental.coroutineContext
+import kotlin.reflect.KProperty
 
 abstract class BaseActivity : DaggerAppCompatActivity(),BaseView {
 
@@ -21,8 +24,26 @@ abstract class BaseActivity : DaggerAppCompatActivity(),BaseView {
     fun launchActivityWithFinish(intent: Intent){
         startActivity(intent)
         finish()
+
     }
 
+    class MyAnimatingView : View(coroutineContext) {
+        // delegated property.
+        // Uses the getter and setter defined in InvalidateDelegate
+        var foregroundX by InvalidateDelegate("dupa")
+
+    }
+    // A View Delegate which invalidates
+// View.postInvalidateOnAnimation when set.
+    class InvalidateDelegate<T : Any>(var value: T) {
+        operator fun getValue(thisRef: View,
+                              property: KProperty<*>) = value
+        operator fun setValue(thisRef: View,
+                              property: KProperty<*>, value: T) {
+            this.value = value
+            thisRef.postInvalidateOnAnimation()
+        }
+    }
 
     abstract fun attachPresenter()
 
